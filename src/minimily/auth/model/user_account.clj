9a7e500a29517efe993d86d428e5baaa284a6bf1
Hildeberto Mendonca <me@hildeberto.com>
2017-09-07 23:47:29 +0200

    The function authenticate returning a map of the authenticated user

--- src/minimily/auth/model/user_account.clj
+++ src/minimily/auth/model/user_account.clj
@@ -6,15 +6,18 @@
 (defn get-it [id]
   (db/get-record table id))
 
-(defn save [record]
-  (db/save-record table record))
+(defn save [a-user-account]
+  (db/save-record table a-user-account))
 
 (defn delete [id]
   (db/delete-record table id))
 
 (defn authenticate [username password]
-  (let [auth_user (db/find-records [(str "select * from " 
-                                      (name table) 
-                                      " where username = ? and password = ?")
-                                    username password])]
-    (not (empty? authenticated_user))))
+  (let [auth_user (db/find-records
+                    [(str "select * from "
+                          (name table)
+                          " where username = ? and password = ?")
+                     username password])]
+    (if (empty? auth_user)
+      nil
+      (dissoc (first auth_user) :password :registration_date))))
