572ba70af31133c80d5424fca2438f419df0ff32
Hildeberto Mendonca <me@hildeberto.com>
2017-10-30 22:19:48 +0100

    Added minimal lein version.

--- project.clj
+++ project.clj
@@ -18,6 +18,7 @@
                  [compojure                 "1.4.0" ]
                  [ring/ring-jetty-adapter   "1.4.0" ]
                  [environ                   "1.0.0" ]] 
+  :min-lein-version "2.0.0"
   :plugins [[environ/environ.lein "0.3.1"]]
   :hooks [environ.leiningen.hooks]
   :uberjar-name "minimily-standalone.jar"
