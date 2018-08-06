ffe1fc5abfeed228f5b0f9ded2294edaaea9be37
Hildeberto Mendonca <me@hildeberto.com>
2018-08-06 18:04:23 -0400

    Standardized routing files.

--- src/minimily/family/web/routing.clj
+++ src/minimily/family/web/routing.clj
@@ -1,31 +1,31 @@
 (ns minimily.family.web.routing
-  (:require [compojure.core                         :as core]
+  (:require [compojure.core                         :as url]
             [ring.middleware.params                 :refer [wrap-params]]
             [minimily.family.web.ctrl.family        :as family-ctrl]
             [minimily.family.web.ctrl.family-member :as family-member-ctrl]))
 
 (defn routes []
-  (core/routes
-    (core/context "/family" []
-      (core/GET  "/"         {session :session} 
+  (url/routes
+    (url/context "/family" []
+      (url/GET  "/"         {session :session} 
                              (family-ctrl/view-family session))
-      (core/GET  "/new"      {session :session {parent :parent} :params} 
+      (url/GET  "/new"      {session :session {parent :parent} :params} 
                              (family-ctrl/new-family session))
-      (core/GET  "/edit"     {session :session}
+      (url/GET  "/edit"     {session :session}
                              (family-ctrl/edit-family session))
-      (core/POST "/save"     {session :session params :params} 
+      (url/POST "/save"     {session :session params :params} 
                              (family-ctrl/save-family session params))
-      (core/POST "/delete"   {session :session params :params} 
+      (url/POST "/delete"   {session :session params :params} 
                              (family-ctrl/delete-family session params))
       
-      (core/context "/family/members" []
-        (core/GET  "/new"      {session :session {family :family} :params}
+      (url/context "/family/members" []
+        (url/GET  "/new"      {session :session {family :family} :params}
                                (family-member-ctrl/new-family-member session family))
-        (core/POST "/save"     {session :session params :params} 
+        (url/POST "/save"     {session :session params :params} 
                                (family-member-ctrl/save-family-member session params))
-        (core/POST "/delete"   {session :session params :params}
+        (url/POST "/delete"   {session :session params :params}
                                (family-member-ctrl/delete-family-member session params))
-        (core/GET  "/:id"      {session :session {id :id} :params}
+        (url/GET  "/:id"      {session :session {id :id} :params}
                                (family-member-ctrl/view-family-member session id))
-        (core/GET  "/:id/edit" {session :session {id :id} :params}
+        (url/GET  "/:id/edit" {session :session {id :id} :params}
                                (family-member-ctrl/edit-family-member session id))))))
