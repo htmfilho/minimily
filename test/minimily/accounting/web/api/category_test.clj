ecf40c18286563afd24f8a3cebf6845c4607b3b5
Hildeberto Mendonca <me@hildeberto.com>
2018-12-08 21:02:45 -0500

    Added some extra test cases to list-tree-branches.

--- test/minimily/accounting/web/api/category_test.clj
+++ test/minimily/accounting/web/api/category_test.clj
@@ -3,6 +3,7 @@
             [minimily.accounting.web.api.category :refer :all]))
 
 (deftest test-list-tree-branches
+  ; The creation of a flat tree based on a list containing hierachy data.
   (testing "Empty category"
     (is (= (list-tree-branches []) [])))
 
@@ -18,12 +19,28 @@
            [{:id 1 :parent nil}
             {:id 2 :parent {:id 1 :parent nil}}])))
     
-  (testing "The creation of a flat tree based on a list containing hierachy data."
+  (testing "A tree of height 3."
     (is (= (list-tree-branches [{:id 1 :parent nil}
                                 {:id 2 :parent 1}
                                 {:id 3 :parent 1}
-                                {:id 5 :parent 2}])
+                                {:id 4 :parent 2}])
            [{:id 1 :parent nil}
             {:id 2 :parent {:id 1 :parent nil}}
             {:id 3 :parent {:id 1 :parent nil}}
-            {:id 5 :parent {:id 2 :parent {:id 1 :parent nil}}}]))))
+            {:id 4 :parent {:id 2 :parent {:id 1 :parent nil}}}])))
+  
+  (testing "2 trees."
+    (is (= (list-tree-branches [{:id 1 :parent nil}
+                                {:id 2 :parent 1}
+                                {:id 3 :parent 1}
+                                {:id 4 :parent 2}
+                                {:id 5 :parent nil}
+                                {:id 6 :parent 5}
+                                {:id 7 :parent 5}])
+           [{:id 1 :parent nil}
+            {:id 5 :parent nil}
+            {:id 2 :parent {:id 1 :parent nil}}
+            {:id 3 :parent {:id 1 :parent nil}}
+            {:id 4 :parent {:id 2 :parent {:id 1 :parent nil}}}
+            {:id 6 :parent {:id 5 :parent nil}}
+            {:id 7 :parent {:id 5 :parent nil}}]))))
