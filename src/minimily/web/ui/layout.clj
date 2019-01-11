842ed323c97d0a202a30880ec5cd8afb21f7afc6
Hildeberto Mendonca <me@hildeberto.com>
2019-01-10 23:11:52 -0500

    Added favicon.ico

--- src/minimily/web/ui/layout.clj
+++ src/minimily/web/ui/layout.clj
@@ -18,9 +18,18 @@
 (defn layout [session title & content]
   (page/html5 {:lang "en"}
     [:head
+      [:link {:rel "apple-touch-icon" :sizes "152x152" :href "/apple-touch-icon.png"}]
+      [:link {:rel "icon" :type "image/png" :sizes "32x32" :href "/favicon-32x32.png"}]
+      [:link {:rel "icon" :type "image/png" :sizes "16x16" :href "/favicon-16x16.png"}]
+      [:link {:rel "manifest" :href "/site.webmanifest"}]
+      [:link {:rel "mask-icon" :href "/safari-pinned-tab.svg" :color "#5bbad5"}]
+
+      [:meta {:name "msapplication-TileColor" :content "#ffc40d"}]
+      [:meta {:name "theme-color" :content "#ffc40d"}]
       [:meta {:charset "utf-8"}]
       [:meta {:name "viewport"
               :content "width=device-width, initial-scale=1, shrink-to-fit=no"}]
+
       [:title "Minimily"]
       (page/include-css "/css/bootstrap.min.css"
                         "/css/custom.css")
