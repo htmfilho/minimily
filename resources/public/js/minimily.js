c2bc9e90703969f07c0966b1db3074055aabeb88
Hildeberto Mendonca <me@hildeberto.com>
2020-03-10 22:29:58 -0400

    Focusing on the first field of a form

--- resources/public/js/minimily.js
+++ resources/public/js/minimily.js
@@ -4,3 +4,7 @@ $("#forward").val(urlParams.get('forward'));
 $("#frm_delete").submit(function(event) {
     return confirm("Are you sure you want to delete it?");
 });
+
+$(document).ready(function() {
+    $('form:first *:input[type!=hidden]:first').focus();
+});
