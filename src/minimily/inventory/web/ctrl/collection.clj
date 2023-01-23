f40bfc8b7e83650600e0ff74de49c29ba22a542f
Hildeberto Mendonca <me@hildeberto.com>
2023-01-23 06:10:42 -0500

    All collection pages in the same namespace

--- src/minimily/inventory/web/ctrl/collection.clj
+++ src/minimily/inventory/web/ctrl/collection.clj
@@ -1,28 +1,26 @@
 (ns minimily.inventory.web.ctrl.collection
   (:require [ring.util.response                        :refer [redirect]]
-            [minimily.inventory.web.ui.collection      :refer [collection-page]]
-            [minimily.inventory.web.ui.collections     :refer [collections-page]]
-            [minimily.inventory.web.ui.collection-form :refer [collection-form-page]]
+            [minimily.inventory.web.ui.collection      :as collection-view]
             [minimily.inventory.model.collection       :as collection-model]
             [minimily.inventory.model.good             :as good-model]))
 
 (defn view-collections [session]
   (let [collections (collection-model/find-all (:profile-id session))]
-    (collections-page session collections)))
+    (collection-view/collections-page session collections)))
 
 (defn view-collection [session id]
   (let [collection-id (Integer/parseInt id)
         collection    (collection-model/get-it (:profile-id session) collection-id)
         goods         (good-model/find-by-collection (:profile-id session) collection-id)]
-    (collection-page session collection goods)))
+    (collection-view/collection-page session collection goods)))
 
 (defn new-collection [session]
-  (collection-form-page session))
+  (collection-view/collection-form-page session))
 
 (defn edit-collection [session id]
   (let [collection-id (Integer/parseInt id)
         collection    (collection-model/get-it (:profile-id session) collection-id)]
-    (collection-form-page session collection)))
+    (collection-view/collection-form-page session collection)))
 
 (defn save-collection [session params]
   (let [collection (conj params {:profile (:profile-id session)})
--- src/minimily/inventory/web/ui/collection.clj
+++ src/minimily/inventory/web/ui/collection.clj
@@ -1,6 +1,6 @@
 (ns minimily.inventory.web.ui.collection
-  (:require [hiccup.form                :refer [form-to submit-button 
-                                                hidden-field]]
+  (:require [hiccup.form                :refer [form-to submit-button label text-field
+                                                text-area hidden-field]]
             [minimily.web.ui.layout     :refer [layout]]
             [minimily.web.ui.bootstrap  :refer [show-field back-button edit-button]]
             [minimily.utils.date        :refer [to-string]]
@@ -34,3 +34,40 @@
             (map #(vector :tr [:td [:a {:href (str "/inventory/goods/" (:id %))} 
                                        (:name %)]]
                               [:td (:description %)]) goods)]]])))
+
+(defn collections-page [session collections]
+  (http-headers 
+    (layout session "Collections"
+      [:div {:class "card"}
+        [:div {:class "card-header"}
+          (back-button "/inventory")
+          (str "&nbsp;")
+          [:a {:href "/inventory/collections/new" :class "btn btn-secondary"} "New Collection"]]
+        [:table {:class "table table-striped"}
+          [:thead
+            [:tr 
+              [:th "Name"]
+              [:th "Description"]]]
+          [:tbody 
+            (map #(vector :tr [:td [:a {:href (str "/inventory/collections/" (:id %))} 
+                                       (:name %)]]
+                              [:td (:description %)]) collections)]]])))
+
+(defn collection-form-page [session & [collection]]
+  (http-headers
+    (layout session "Collection"
+      (form-to [:post "/inventory/collections/save"]
+        (when collection (hidden-field "id" (:id collection)))
+        [:div {:class "form-group"}
+          (label "name" "Name")
+          (text-field {:class "form-control" :id "name"} 
+                      "name" 
+                      (:name collection))]
+        [:div {:class "form-group"}
+          (label "description" "Description")
+          (text-area {:class "form-control" :id "description"} 
+                      "description"
+                      (:description collection))]
+        (submit-button {:class "btn btn-primary"} "Submit")
+        (str "&nbsp;")
+        [:a {:class "btn btn-outline-secondary" :href (str "/inventory/collections/" (:id collection))} "Cancel"]))))
